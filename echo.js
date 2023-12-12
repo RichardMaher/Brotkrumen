6b365dd30bcb68fe5a0725e3814e92ccc73525e9
Richard Maher <maherrj@gmail.com>
2023-12-12 11:36:26 +0800

    Refresh Cache version

--- echo.js
+++ echo.js
@@ -7,7 +7,7 @@
 var   doryRegister  = [];            // Meet someone new every day.
 const INTRO         = "GrussGott";   // Tell clients we're new.
 const FLEET_MANAGER = "/Fleet/Move"; // Starship control.
-const CACHE_NAME    = "BrotkrumenV1.3"
+const CACHE_NAME    = "BrotkrumenV1.4"
 	
 self.addEventListener('install',
     function (e) {
